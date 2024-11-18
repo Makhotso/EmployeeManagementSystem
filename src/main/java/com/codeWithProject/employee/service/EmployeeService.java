238893362ec53b1e4f9e0c431dcebdd6b4bc2e43
employee <thato.makhotso@gmail.com>
2024-11-18 16:27:55 +0200

    Second Commit

--- src/main/java/com/codeWithProject/employee/service/EmployeeService.java
+++ src/main/java/com/codeWithProject/employee/service/EmployeeService.java
@@ -2,8 +2,11 @@ package com.codeWithProject.employee.service;
 
 import com.codeWithProject.employee.entity.Employee;
 import com.codeWithProject.employee.repository.EmployeeRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,9 +14,17 @@ public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
 
-    public Employee postEmployee (Employee employee){
+    public Employee postEmployee(Employee employee) {
         return employeeRepository.save(employee);
     }
 
+    public List<Employee> getAllEmployees() {return employeeRepository.findAll();}
 
+    public void deleteEmployee(Long id){
+        if (!employeeRepository.existsById(id)) {
+            throw new EntityNotFoundException("Employee with ID" + id + "not found");
+        }
+
+        employeeRepository.deleteById(id);
+    }
 }
